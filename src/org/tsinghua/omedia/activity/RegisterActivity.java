b1f3aaae9b53804bbcde973a4b32c30bebe2ff8b
czw <346402360@qq.com>
2012-03-12 15:43:15 +0800

    Add register
    RT

--- src/org/tsinghua/omedia/activity/RegisterActivity.java
+++ src/org/tsinghua/omedia/activity/RegisterActivity.java
@@ -1,197 +1,99 @@
+
 package org.tsinghua.omedia.activity;
 
+import org.tsinghua.omedia.R;
+import org.tsinghua.omedia.form.RegisterForm;
+import org.tsinghua.omedia.serverAPI.RegisterAPI;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
- * 
- * @author hanfuye
- * 
+ * @author czw
  */
 public class RegisterActivity extends BaseActivity {
 
-//	private static final Logger logger = Logger
-//			.getLogger(LandingActivity.class);
-//	
-//	private static Camera mCamera;
-//	private CameraPreview mPreview;
-//	private Calendar cal;
-//
-//	private String userSex;
-//	private String userImage;
-//
-//	private RegisterForm registerForm;
-//
-//	protected void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.register_activity);
-//
-//		initListener();
-//	}
-//
-//	private void initListener() {
-//		// 照片
-//		register_camera_init();
-//
-//		// 出生日期
-//		register_date_init();
-//		register_date_check();
-//
-//		// 性别
-//		RadioGroup sexgroup = (RadioGroup) findViewById(R.id.register_sexGroup);
-//		register_sex_init(sexgroup);
-//		register_sex_check(sexgroup);
-//
-//		// 注册
-//		Button registerButton = (Button) findViewById(R.id.register_button);
-//		registerButton.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				if (checkRegister()) {
-//					doRegister();
-//				} else {
-//					logger.error( "checkRegister() return false");
-//				}
-//			}
-//
-//		});
-//	}
-//
-//	private void register_camera_init() {
-//
-//		mCamera = getCameraInstance();
-//		mPreview = new CameraPreview(this, mCamera);
-//		FrameLayout preview = (FrameLayout) findViewById(id.camera_view);
-//		preview.addView(mPreview);
-//
-//	}
-//
-//	public static Camera getCameraInstance() {
-//		Camera cam = null;
-//		try {
-//			cam = Camera.open(1);
-//		} catch (Exception e) {
-//			logger.error( "getCameraInstance()" + e.toString());
-//		}
-//		return cam;
-//
-//	}
-//
-//	private void register_sex_init(RadioGroup sexgroup) {
-//		int radioButtonId = sexgroup.getCheckedRadioButtonId();
-//		RadioButton rb = (RadioButton) RegisterActivity.this
-//				.findViewById(radioButtonId);
-//		userSex = rb.getTag() + "";
-//	}
-//
-//	private void register_sex_check(RadioGroup sexgroup) {
-//		sexgroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-//			@Override
-//			public void onCheckedChanged(RadioGroup arg0, int arg1) {
-//				// TODO Auto-generated method stub
-//
-//				int radioButtonId = arg0.getCheckedRadioButtonId();
-//
-//				RadioButton rb = (RadioButton) RegisterActivity.this
-//						.findViewById(radioButtonId);
-//				userSex = rb.getTag() + "";
-//			}
-//
-//		});
-//	}
-//
-//	private void register_date_init() {
-//		cal = Calendar.getInstance();
-//	}
-//
-//	private void register_date_check() {
-//		Button selectDatebutton = (Button) findViewById(R.id.register_selectdate);
-//		selectDatebutton.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				new DatePickerDialog(RegisterActivity.this,
-//						new OnDateSetListener() {
-//							@Override
-//							public void onDateSet(DatePicker arg0, int arg1,
-//									int arg2, int arg3) {
-//								cal.set(Calendar.YEAR, arg1);
-//								cal.set(Calendar.MONTH, arg2);
-//								cal.set(Calendar.DAY_OF_MONTH, arg3);
-//								updateDate();
-//							}
-//						}, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal
-//								.get(Calendar.DAY_OF_MONTH)).show();
-//			}
-//		});
-//	}
-//
-//	private void updateDate() {
-//		SimpleDateFormat df = new SimpleDateFormat("yyyy年MM月dd日");
-//		EditText et = (EditText) findViewById(R.id.register_dateview);
-//		String temp = df.format(cal.getTime());
-//		et.setText(temp);
-//	}
-//
-//	private boolean checkRegister() {
-//		// TODO Auto-generated method stub
-//		if (userImage != null) {
-//			alert(1, "好像还没有图像呢");
-//			return false;
-//		}
-//		return true;
-//	}
-//
-//	private void doRegister() {
-//
-//
-//		EditText name = (EditText) findViewById(R.id.username_register);
-//		EditText email = (EditText) findViewById(R.id.email_register);
-//		EditText passwd = (EditText) findViewById(R.id.password_register);
-//		EditText Rpasswd = (EditText) findViewById(R.id.password_register_check);
-//
-//		registerForm = new RegisterForm();
-//		registerForm.setUsername(name.getText().toString());
-//		registerForm.setPassword(passwd.getText().toString());
-//		registerForm.setEmail(email.getText().toString());
-//		registerForm.setConfirmPassword(Rpasswd.getText().toString());
-//
-//		new RegisterAPI(registerForm, RegisterActivity.this) {
-//
-//			@Override
-//			protected void onSuccess() {
-//				alert(2, "注册成功");
-//			}
-//		}.call();
-//
-//	}
-//
-//	private void alert(int kind, String string) {
-//		// TODO Auto-generated method stub
-//		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//		switch (kind) {
-//		case 1:
-//			builder.setMessage(string);
-//			builder.setCancelable(false);
-//			builder.setPositiveButton("Yes",
-//					new DialogInterface.OnClickListener() {
-//						public void onClick(DialogInterface dialog, int id) {
-//
-//						}
-//					});
-//			break;
-//		case 2:
-//			builder.setMessage(string);
-//			builder.setCancelable(false);
-//			builder.setPositiveButton("Yes",
-//					new DialogInterface.OnClickListener() {
-//						public void onClick(DialogInterface dialog, int id) {
-//							Intent intent = new Intent();
-//							intent.setClass(RegisterActivity.this,
-//									LandingActivity.class);
-//							startActivity(intent);
-//						}
-//					});
-//			break;
-//		}
-//		AlertDialog ad = builder.create();
-//		ad.show();
-//	}
+    private Button resetButton;
+
+    private Button okButton;
+
+    private EditText userNameText;
+
+    private EditText pswText;
+
+    private EditText confirmPswText;
+
+    private EditText nameText;
+
+    private EditText emailText;
+
+    private EditText addressText;
+
+    private EditText phoneNumberText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_view);
+
+        resetButton = (Button) findViewById(R.id.register_reset_id);
+        okButton = (Button) findViewById(R.id.register_ok);
+        userNameText = (EditText) findViewById(R.id.register_username_id);
+        pswText = (EditText) findViewById(R.id.register_psw);
+        confirmPswText = (EditText) findViewById(R.id.register_check_psw);
+        nameText = (EditText) findViewById(R.id.register_real_name);
+        emailText = (EditText) findViewById(R.id.register_email_id);
+        addressText = (EditText) findViewById(R.id.register_address_id);
+        phoneNumberText = (EditText) findViewById(R.id.register_phone_id);
+
+        initListener();
+
+    }
+
+    private void initListener() {
+        okButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                RegisterForm form = new RegisterForm();
+                form.setUsername(userNameText.getText().toString());
+                form.setPassword(pswText.getText().toString());
+                form.setConfirmPassword(confirmPswText.getText().toString());
+                form.setEmail(emailText.getText().toString());
+                doRegisterInfo(form);
+            }
+        });
+
+        resetButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                userNameText.setText("");
+                pswText.setText("");
+                confirmPswText.setText("");
+                nameText.setText("");
+                emailText.setText("");
+                addressText.setText("");
+                phoneNumberText.setText("");
+            }
+        });
+
+    }
+
+    private void doRegisterInfo(RegisterForm form) {
+        new RegisterAPI(form, this) {
+
+            @Override
+            protected void onSuccess() {
+                Toast.makeText(RegisterActivity.this, R.string.register_success, 0);
+                // TODO landing?
+                RegisterActivity.this.finish();
+            }
+
+        }.call();
+    }
 }
