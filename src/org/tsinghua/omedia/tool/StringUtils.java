a56d8d8baaac35da0d79121597c1b632d9ce7d8b
xuhongfeng <xuhongfeng@wandoujia.com>
2012-03-13 16:34:55 +0800

    tmp

--- src/org/tsinghua/omedia/tool/StringUtils.java
+++ src/org/tsinghua/omedia/tool/StringUtils.java
@@ -1,5 +1,6 @@
 package org.tsinghua.omedia.tool;
 
+import java.io.UnsupportedEncodingException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,7 +10,8 @@ import java.util.regex.Pattern;
  *
  */
 public class StringUtils {
-
+    private static final Logger logger = Logger.getLogger(StringUtils.class);
+    
     private static final Pattern EMAIL_PATTERN = 
             Pattern.compile("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*" +
                     "@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
@@ -23,5 +25,23 @@ public class StringUtils {
     public static boolean isAlphaOrNum(String s) {
         Matcher m = ALPHA_NUM_PATTERN.matcher(s);
         return m.matches();
+    }
+    
+    public static String utf8toiso88591(String s) {
+        try {
+            return new String(s.getBytes("utf8"), "ISO8859_1");
+        } catch (UnsupportedEncodingException e) {
+            logger.error(e);
+            return null;
+        }
+    }
+    
+    public static String iso88591toutf8(String s) {
+        try {
+            return new String(s.getBytes("ISO8859_1"), "utf8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error(e);
+            return null;
+        }
     }
 }
